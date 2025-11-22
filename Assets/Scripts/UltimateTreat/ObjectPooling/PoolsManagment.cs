23d11ee42fa4640b4cbb6b8c382f0c5db8776285
LuisVivancoG <48226613+LuisVivancoG@users.noreply.github.com>
2025-11-22 12:39:12 -0800

    Adjusting visual glitches

--- Assets/Scripts/UltimateTreat/ObjectPooling/PoolsManagment.cs
+++ Assets/Scripts/UltimateTreat/ObjectPooling/PoolsManagment.cs
@@ -23,7 +23,7 @@ public class PoolsManagment : Singleton<PoolsManagment>
         foreach (SOType type in Enum.GetValues(typeof(SOType))) //Adds a pool for each SOType in the _poolsDictionary
         {
             _poolsDictionary.Add(type, new PoolBase(() => CreatePoolObjectType(type), GetObjectFromPool, ReturnObjectToPool));
-            Debug.LogWarning($"Pool initialized: {type} || current dictionary: {_poolsDictionary.Keys}");
+            //Debug.LogWarning($"Pool initialized: {type} || current dictionary: {_poolsDictionary.Keys}");
         }
     }
 
--- Assets/Scripts/UltimateTreat/UI/PlayerSelection/SystemInputsManager.cs
+++ Assets/Scripts/UltimateTreat/UI/PlayerSelection/SystemInputsManager.cs
@@ -1,12 +1,6 @@
 using System.Collections.Generic;
-using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-public class PlayerProfile
-{
-    private int _playerNumber;
-}
 
 public class SystemInputsManager : MonoBehaviour
 {
@@ -14,10 +8,7 @@ public class SystemInputsManager : MonoBehaviour
     [SerializeField] private GameObject _inputsGrp;
 
     private List<PlayerInput> _players = new List<PlayerInput>();
-
-    private bool _firstPlayerTagged = false;
     private int _playersCount = 2;
-    private int _maxPlayers = 4;
 
     //Getters
     public int PlayersCount {  get { return _playersCount; } }
@@ -27,44 +18,9 @@ public class SystemInputsManager : MonoBehaviour
 
     public void OnplayerJoined(PlayerInput playerInput)
     {
-        /*_players.Add(playerInput);
-
-        if (!_firstPlayerTagged)
-        {
-            playerInput.TryGetComponent<PlayerDisplay>(out var script);
-
-        }*/
-
-        Debug.Log($"Initial scale {playerInput.gameObject.transform.localScale}" +
-            $"Initial position {playerInput.gameObject.transform.localPosition}");
-
-        /*playerInput.TryGetComponent<PlayerDisplay>(out var component);
-        PrefabTransformCheck(component.GOTransform);
-
-        Debug.Log($"Initial scale {component.GOTransform.localScale}" +
-            $"Initial position {component.GOTransform.localPosition}");*/
+        /*Debug.Log($"Initial scale {playerInput.gameObject.transform.localScale}" +
+            $"Initial position {playerInput.gameObject.transform.localPosition}");*/
 
         _playersCount++;
     }
-
-    /*void PrefabTransformCheck(RectTransform transform)
-    {
-        transform.localScale = Vector3.one;
-        transform.localPosition = Vector3.zero;
-    }*/
-
-    /*private void Update()
-    {
-        if(Input.GetKeyUp(KeyCode.A))
-        {
-            _playersCount++;
-            Instantiate(_displayPlayer, _playersGrp.transform);
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            _playersCount--;
-            var player = _playersGrp.transform.GetChild(_playersCount - 1);
-            Destroy(player.gameObject);
-        }
-    }*/
 }
